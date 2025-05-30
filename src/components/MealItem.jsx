14865018ce7715bd9880cf14dd9db2e28d9c7f83
AshrafElsaghier1 <ashrafelsaghier17@gmail.com>
2025-05-30 18:39:30 +0300

    Implement CartContext for state management; update MealItem and Header components; add currency formatting utility; adjust Margherita Pizza price

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,6 +1,16 @@
-import { formattingPrice } from "../utils/formattingPrice";
+import { useContext } from 'react';
+
+import { currencyFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -8,12 +18,12 @@ export default function MealItem({ meal }) {
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
-            {formattingPrice.format(meal.price)}
+            {currencyFormatter.format(meal.price)}
           </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
